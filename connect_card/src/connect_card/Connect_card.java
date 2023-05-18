0b4b6542da33c97bf7887fadf7184156a2aedb2b
Tu070901 <110175079+Tu070901@users.noreply.github.com>
2023-05-18 11:29:44 +0700

    a

---
+++ connect_card/src/connect_card/Connect_card.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect_card;
+
+/**
+ *
+ * @author daotu
+ */
+public class Connect_card {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+      view manHinh= new view();
+      manHinh.setVisible(true);
+    }
+    
+}
